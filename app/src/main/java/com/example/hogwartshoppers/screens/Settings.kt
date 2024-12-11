d4ed0f1a2e6cc57215b68380aaf9656dd6e7cf27
guisousa02 <guifilipesousasilva@gmail.com>
2024-12-11 15:21:29 +0000

    added menu to settings

--- app/src/main/java/com/example/hogwartshoppers/screens/Settings.kt
+++ app/src/main/java/com/example/hogwartshoppers/screens/Settings.kt
@@ -8,26 +8,35 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,6 +52,10 @@ import androidx.compose.ui.unit.sp
 import com.example.hogwartshoppers.R
 import com.example.hogwartshoppers.ui.theme.HogwartsHoppersTheme
 import kotlinx.coroutines.NonCancellable.start
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import kotlinx.coroutines.launch
 
 @Composable
 fun SettingsScreen() {
@@ -52,121 +65,197 @@ fun SettingsScreen() {
     val isIconsVisible = remember { mutableStateOf(false) }
     val isSafetyFeatureEnabled = remember { mutableStateOf(true) }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xff321f12))
-            .border(3.dp, Color(0xFFBB9753))
-            .padding(16.dp)
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier = Modifier
+                    .fillMaxHeight() // Ensure it fills the available height
+                    .width(300.dp)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize() // Box fills the available space
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Button(onClick = {
+                            // Handle Log-In click
+                        }) {
+                            Text("Log-In")
+                        }
+                        Button(onClick = {
+                            // Handle Sign-Up click
+                        }) {
+                            Text("Sign-Up")
+                        }
+                        Button(onClick = {
+                            // Handle Profile click
+                        }) {
+                            Text("Profile")
+                        }
+                        Button(onClick = {
+                            // Handle HomePage click
+                        }) {
+                            Text("HomePage")
+                        }
+                    }
+                }
+
+            }
+        },
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.hogwartslogo),
-            contentDescription = "Hogwarts Logo",
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .size(200.dp) // Adjust size as needed
-                .padding(bottom = 15.dp)
-        )
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-                .padding(top = 80.dp),
-            verticalArrangement = Arrangement.Center
+        Scaffold(
+            floatingActionButton = {
+                Box(
+                    modifier = Modifier.fillMaxSize() // Box to fill available space
+                ) {
+                    ExtendedFloatingActionButton(
+                        text = { Text("") },
+                        icon = { Icon(Icons.Filled.Menu,
+                            contentDescription = "Menu",
+                            modifier = Modifier.size(60.dp))},
+                        onClick = {
+                            scope.launch {
+                                drawerState.apply {
+                                    if (isClosed) open() else close()
+                                }
+                            }
+                        },
+                        modifier = Modifier
+                            .padding(start = 20.dp, top = 20.dp)
+                            .align(Alignment.TopStart)
+                            .width(100.dp)
+                            .height(100.dp),
+                        containerColor = Color(0xff321f12), // Change the button color (purple in this case)
+                        contentColor = Color.White // Change the icon color to white
+                    )
+                }
+            }
         ) {
             Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .size(350.dp, 70.dp) // Set specific width and height
-                    .padding(bottom = 30.dp)
-                    .background(
-                        color = Color(0xff4b2f1b), // Brown background
-                        shape = RoundedCornerShape(16.dp) // Makes corners rounded
-                    ),
-                contentAlignment = Alignment.Center // Centers the text inside the box
-
+                    .fillMaxSize()
+                    .background(Color(0xff321f12))
+                    .border(3.dp, Color(0xFFBB9753))
+                    .padding(16.dp)
             ) {
-                Text(
-                    text = "Settings",
-                    color = Color.White
+                Image(
+                    painter = painterResource(id = R.drawable.hogwartslogo),
+                    contentDescription = "Hogwarts Logo",
+                    modifier = Modifier
+                        .align(Alignment.TopCenter)
+                        .size(200.dp) // Adjust size as needed
+                        .padding(bottom = 15.dp)
                 )
-            }
-
-
-            Column(
-                modifier = Modifier
-                    .size(350.dp, 500.dp)
-                    .background(
-                        Color(0xffe9dbc0),
-                        shape = RoundedCornerShape(16.dp)
-                    ), // Background and rounded corners
-                verticalArrangement = Arrangement.spacedBy(16.dp), // Adds space between buttons
-                horizontalAlignment = Alignment.CenterHorizontally // Centers the buttons horizontally
-            ) {
-
-                Spacer(modifier = Modifier.height(28.dp))
-                SettingRow("Turn OFF/ON Notifications", isNotificationsEnabled)
-                SettingRow("Show icons while flying", isIconsVisible)
-                SettingRow("Safety Features", isSafetyFeatureEnabled)
-
-                Text(text = "Enter Coupon:",
-                    color = Color(0xff4b2f1b),
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier
-                        .align(Alignment.Start)
-                        .padding(start = 30.dp),
-                    style = TextStyle(
-                        fontSize = 22.sp // Specify the size in `sp` (scale-independent pixels)
-                    )
-                )
-
-                TextField(
-                    value = coupon,
-                    onValueChange = { coupon = it },
-                    label = { Text("Coupon") },
-                    visualTransformation = PasswordVisualTransformation(),
-                    modifier = Modifier
-                        .size(275.dp, 50.dp),
-                    shape = RoundedCornerShape(30.dp) // This makes the corners rounded
-                )
-
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .size(275.dp, 50.dp)
-                        .fillMaxWidth()
-                        .padding(top = 16.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xff4b2f1b)
-                    )
+                        .fillMaxSize()
+                        .padding(16.dp)
+                        .padding(top = 80.dp),
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Text("Payment Options")
-                }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .size(350.dp, 70.dp) // Set specific width and height
+                            .padding(bottom = 30.dp)
+                            .background(
+                                color = Color(0xff4b2f1b), // Brown background
+                                shape = RoundedCornerShape(16.dp) // Makes corners rounded
+                            ),
+                        contentAlignment = Alignment.Center // Centers the text inside the box
 
-                // Sign Out Button
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .size(275.dp, 35.dp)
-                        .fillMaxWidth(),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xff4b2f1b)
-                    )
-                ) {
-                    Text("Sign Out")
-                }
+                    ) {
+                        Text(
+                            text = "Settings",
+                            color = Color.White
+                        )
+                    }
 
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .size(275.dp, 50.dp)
-                        .fillMaxWidth()
-                        .padding(bottom = 16.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xffe22134)
-                    )
-                ) {
-                    Text("AVADA KEDAVRA your account")
+
+                    Column(
+                        modifier = Modifier
+                            .size(350.dp, 500.dp)
+                            .background(
+                                Color(0xffe9dbc0),
+                                shape = RoundedCornerShape(16.dp)
+                            ), // Background and rounded corners
+                        verticalArrangement = Arrangement.spacedBy(16.dp), // Adds space between buttons
+                        horizontalAlignment = Alignment.CenterHorizontally // Centers the buttons horizontally
+                    ) {
+
+                        Spacer(modifier = Modifier.height(28.dp))
+                        SettingRow("Turn OFF/ON Notifications", isNotificationsEnabled)
+                        SettingRow("Show icons while flying", isIconsVisible)
+                        SettingRow("Safety Features", isSafetyFeatureEnabled)
+
+                        Text(text = "Enter Coupon:",
+                            color = Color(0xff4b2f1b),
+                            modifier = Modifier
+                                .align(Alignment.Start)
+                                .padding(start = 30.dp),
+                            style = TextStyle(
+                                fontSize = 22.sp // Specify the size in `sp` (scale-independent pixels)
+                            )
+                        )
+
+                        TextField(
+                            value = coupon,
+                            onValueChange = { coupon = it },
+                            label = { Text("Coupon") },
+                            visualTransformation = PasswordVisualTransformation(),
+                            modifier = Modifier
+                                .size(275.dp, 50.dp),
+                            shape = RoundedCornerShape(30.dp) // This makes the corners rounded
+                        )
+
+                        Button(
+                            onClick = {},
+                            modifier = Modifier
+                                .size(275.dp, 50.dp)
+                                .fillMaxWidth()
+                                .padding(top = 16.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xff4b2f1b)
+                            )
+                        ) {
+                            Text("Payment Options")
+                        }
+
+                        // Sign Out Button
+                        Button(
+                            onClick = {},
+                            modifier = Modifier
+                                .size(275.dp, 35.dp)
+                                .fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xff4b2f1b)
+                            )
+                        ) {
+                            Text("Sign Out")
+                        }
+
+                        Button(
+                            onClick = {},
+                            modifier = Modifier
+                                .size(275.dp, 50.dp)
+                                .fillMaxWidth()
+                                .padding(bottom = 16.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xffe22134)
+                            )
+                        ) {
+                            Text("AVADA KEDAVRA your account")
+                        }
+                    }
                 }
             }
         }
